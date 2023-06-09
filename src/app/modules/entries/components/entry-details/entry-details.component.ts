9795327f2a2db1686a2662d1935f66320b36ac9b
Mateusz Nowak <matteuszn65@gmail.com>
2023-06-09 18:22:31 +0200

    Added basic implementation for entry details

--- src/app/modules/entries/components/entry-details/entry-details.component.ts
+++ src/app/modules/entries/components/entry-details/entry-details.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { EntryType, stringToEntryType } from 'src/app/core';
+import { Subscription } from 'rxjs';
+import {
+  Category,
+  CategoryService,
+  Entry,
+  EntryHttpService,
+  EntryType,
+  stringToEntryType,
+} from 'src/app/core';
 
 @Component({
   selector: 'app-entry-details',
@@ -9,12 +17,54 @@ import { EntryType, stringToEntryType } from 'src/app/core';
 })
 export class EntryDetailsComponent {
   entryType!: EntryType;
+  entry?: Entry;
+  private entrySubscription: Subscription | null = null;
 
-  constructor(private readonly route: ActivatedRoute) {}
+  constructor(
+    private readonly route: ActivatedRoute,
+    private entryHttpService: EntryHttpService,
+    private categoryService: CategoryService
+  ) {}
 
   ngOnInit(): void {
     this.entryType = stringToEntryType(
       this.route.snapshot.paramMap.get('entryType')!
     );
+    this.loadEntry();
+  }
+
+  ngOnDestroy(): void {
+    if (this.entrySubscription) {
+      this.entrySubscription.unsubscribe();
+    }
+  }
+
+  getCategoryClass(category: Category): string[] {
+    return this.categoryService.getCategoryClass(category);
+  }
+
+  getCategoryName(category: Category) {
+    return this.categoryService.getCategoryName(category);
+  }
+
+  loadEntry(): void {
+    // TODO: change to entryHttpService.getEntry
+    if (this.entryType !== null) {
+      this.entrySubscription = this.entryHttpService
+        .getEntries({ type: this.entryType })
+        .subscribe((response) => {
+          this.entry = response.result.find(
+            (entry) =>
+              entry.entry_id === Number(this.route.snapshot.paramMap.get('id'))
+          );
+          console.log(this.entry);
+        });
+    }
+  }
+
+  handleFavorite(e: MouseEvent) {
+    e.stopPropagation();
+    if (!this.entry) return;
+    this.entry.favorite = !this.entry?.favorite;
   }
 }
