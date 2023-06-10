1520c513cb0b4b980ae4334e6cdae7f5714416d9
adabd-pl <72196988+adabd-pl@users.noreply.github.com>
2023-06-10 13:03:17 +0200

    Create entry add view

--- src/app/modules/entries/components/entry-add/entry-add.component.ts
+++ src/app/modules/entries/components/entry-add/entry-add.component.ts
@@ -1,21 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs';
 import { EntryType, stringToEntryType } from 'src/app/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSelect } from '@angular/material/select';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-entry-add',
   templateUrl: './entry-add.component.html',
   styleUrls: ['./entry-add.component.scss']
 })
 export class EntryAddComponent {
-  entryType!: EntryType;
+  entryType: EntryType | null = null;
+  entryTypeString: string | null = null;
+  form: FormGroup = new FormGroup({});
+  hide = true;
 
-  constructor(private readonly route: ActivatedRoute,) { }
+
+  selectedFile: File | undefined;
+  constructor(private readonly route: ActivatedRoute, private fb: FormBuilder, private _location: Location) { }
 
   ngOnInit(): void {
-    this.entryType = stringToEntryType(
-      this.route.snapshot.paramMap.get('entryType')!
-    ); 
+    this.route.paramMap.pipe(
+      first()
+    ).subscribe(
+      paramMap => {
+        const entryType = paramMap.get('entryType');
+        if (entryType) {
+          this.entryType = stringToEntryType(entryType);
+          this.entryTypeString = entryType
+        }
+      }
+    );
+    this.form = this.fb.group({
+      title: [null, [Validators.required]],
+      details: [null, [Validators.required]]
+    });
+  }
+
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0] ?? null;
+
+  }
+
+  backClicked() {
+    this._location.back();
   }
 }
