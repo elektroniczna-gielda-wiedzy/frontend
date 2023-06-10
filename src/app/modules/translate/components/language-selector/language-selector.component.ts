03c1f0e1fee7fde1ba46b6bb4e7a2ff3ac3b05b5
Mateusz Nowak <matteuszn65@gmail.com>
2023-06-10 14:21:25 +0200

    Styled language selector

--- src/app/modules/translate/components/language-selector/language-selector.component.ts
+++ src/app/modules/translate/components/language-selector/language-selector.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LanguageService } from '../../language.service';
+import { Language } from 'src/app/core';
 
 @Component({
   selector: 'app-language-selector',
@@ -7,14 +8,32 @@ import { LanguageService } from '../../language.service';
   styleUrls: ['./language-selector.component.scss']
 })
 export class LanguageSelectorComponent {
+  @Input() sideNav: boolean = false;
 
   constructor(private languageService: LanguageService) { }
 
-  get language(): number {
+  get currentLanguage(): Language {
     return this.languageService.language;
   }
 
   setLanguage(lang: number) {
     this.languageService.language = lang;
   }
+
+  get polish(): Language {
+    return Language.pl
+  }
+
+  get english(): Language {
+    return Language.en
+  }
+
+  get polishSelector(): string {
+    return Language[Language.pl].toUpperCase();
+  }
+
+  get englishSelector(): string {
+    return Language[Language.en].toUpperCase();
+  }
+
 }
--- src/app/modules/translate/translate.module.ts
+++ src/app/modules/translate/translate.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageSelectorComponent } from './components/language-selector/language-selector.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [LanguageSelectorComponent],
-  imports: [TranslateModule],
+  imports: [TranslateModule, MatButtonModule],
   exports: [TranslateModule, LanguageSelectorComponent],
 })
 export class AppTranslateModule {}
