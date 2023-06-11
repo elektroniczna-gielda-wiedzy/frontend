c15776062787418bb8633564478f5b01a2de2475
Mateusz Nowak <matteuszn65@gmail.com>
2023-06-11 20:43:05 +0200

    fixed tokenDisallowedRoutes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ import { NgxLoggerLevel } from 'ngx-logger';
 export const environment = {
   production: false,
   tokenAllowedDomains: ['localhost:8080'],
-  tokenDisallowedRoutes: ['localhost:8080/api/auth'],
+  tokenDisallowedRoutes: ['http://localhost:8080/api/v1/auth/sign_in', 'http://localhost:8080/api/v1/auth/sign_up'],
   apiUrl: 'http://localhost:8080/api/v1',
   defaultLanguage: 'pl',
   logLevel: NgxLoggerLevel.TRACE,
