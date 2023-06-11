97bce1fbb36cabb493501a1c1f57d6bcfdc21b1d
Mateusz Nowak <matteuszn65@gmail.com>
2023-06-11 20:43:58 +0200

    auto logout when auth guard fails

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,16 +1,17 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { TokenService } from '../services/token.service';
+import { AuthService } from '../services/auth.service';
 
 
 export const authGuard: CanActivateFn = (
   route,
   state,
-  router = inject(Router),
-  tokenService = inject(TokenService)
+  tokenService = inject(TokenService),
+  authService = inject(AuthService)
 ) => {
   if (tokenService.isTokenExpired()) {
-    router.navigate(['auth', 'sign-in']);
+    authService.logout();
     return false;
   }
   return true;
