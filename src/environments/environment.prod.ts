96c92be41d8c101ac06fcab56d4d1268b401d0c9
Mateusz Furga <mfurga@student.agh.edu.pl>
2023-06-23 15:04:52 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,9 @@ import { NgxLoggerLevel } from 'ngx-logger';
 
 export const environment = {
   production: true,
-  apiUrl: 'TODO: Add your production API URL here',
+  tokenAllowedDomains: ['localhost:8080'],
+  tokenDisallowedRoutes: ['http://localhost:8080/api/v1/auth/sign_in', 'http://localhost:8080/api/v1/auth/sign_up'],
+  apiUrl: 'http://20.251.9.203:8080/api/v1',
   defaultLanguage: 'pl',
   logLevel: NgxLoggerLevel.OFF,
   serverLogLevel: NgxLoggerLevel.OFF,
