9eae3d5e6723382644e2c405a2383253063f1c12
Mateusz Nowak <matteuszn65@gmail.com>
2023-06-26 23:56:20 +0200

    Added order param and formatted file

--- src/app/core/http/entry-http.service.ts
+++ src/app/core/http/entry-http.service.ts
@@ -9,17 +9,21 @@ import { ENTRY_TYPES } from '../mocks/entry_type';
 import { TokenService } from '../services/token.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EntryHttpService {
   private readonly apiUrl = `${environment.apiUrl}/entry`;
 
-  constructor(private http: HttpClient, private tokenService: TokenService) { }
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  getEntries(params: {type?: EntryType, categories?: Number[], query?: string}) : Observable<StandardResponse<Entry>>{
+  getEntries(params: {
+    type?: EntryType;
+    categories?: Number[];
+    query?: string;
+  }): Observable<StandardResponse<Entry>> {
     const url = this.apiUrl;
     let queryParams = {
-      params: new HttpParams()
+      params: new HttpParams(),
     };
 
     if (params.type) {
@@ -27,85 +31,87 @@ export class EntryHttpService {
     }
 
     if (params.categories && params.categories.length > 0) {
-      queryParams.params = queryParams.params.set('category_ids', params.categories.join(','));
+      queryParams.params = queryParams.params.set(
+        'category_ids',
+        params.categories.join(',')
+      );
     }
 
     if (params.query) {
       queryParams.params = queryParams.params.set('query', params.query);
     }
 
-    return this.http.get<StandardResponse<Entry>>(url, queryParams);
+    queryParams.params = queryParams.params.set('order', 'DESC');
 
+    return this.http.get<StandardResponse<Entry>>(url, queryParams);
   }
 
-  getMyEntries() : Observable<StandardResponse<Entry>>{
+  getMyEntries(): Observable<StandardResponse<Entry>> {
     const url = this.apiUrl;
     let queryParams = {
-      params: new HttpParams()
+      params: new HttpParams(),
     };
 
     const userId = this.tokenService.getUserId();
     if (userId) {
       queryParams.params = queryParams.params.set('author', userId);
     } else {
-      return of({result: [], messages: ["No user id found"], success: false});
+      return of({ result: [], messages: ['No user id found'], success: false });
     }
-    
 
     return this.http.get<StandardResponse<Entry>>(url, queryParams);
   }
 
-  getMyFavorites() : Observable<StandardResponse<Entry>>{
+  getMyFavorites(): Observable<StandardResponse<Entry>> {
     const url = this.apiUrl;
     let queryParams = {
-      params: new HttpParams()
+      params: new HttpParams(),
     };
 
     const userId = this.tokenService.getUserId();
     if (userId) {
       queryParams.params = queryParams.params.set('favorites', true);
-      // queryParams.params = queryParams.params.set('author', userId);
     } else {
-      return of({result: [], messages: ["No user id found"], success: false});
+      return of({ result: [], messages: ['No user id found'], success: false });
     }
 
     return this.http.get<StandardResponse<Entry>>(url, queryParams);
   }
 
-
-  createEntry(params: EntryRequest) : Observable<StandardResponse<Entry>>{
+  createEntry(params: EntryRequest): Observable<StandardResponse<Entry>> {
     const url = this.apiUrl;
-    
-    return this.http.post<StandardResponse<Entry>>(url, params);
 
+    return this.http.post<StandardResponse<Entry>>(url, params);
   }
 
-  getEntry(id: number) : Observable<StandardResponse<Entry>>{
+  getEntry(id: number): Observable<StandardResponse<Entry>> {
     const url = `${this.apiUrl}/${id}`;
 
     return this.http.get<StandardResponse<Entry>>(url);
   }
-  
 
-  getCategoryTypeHint(entry_type_id: EntryType | undefined){
-    let result = "";
+  getCategoryTypeHint(entry_type_id: EntryType | undefined) {
+    let result = '';
 
-    ENTRY_TYPES.forEach( c => {if(entry_type_id === c.entry_type_id) result = c.hint });
-
+    ENTRY_TYPES.forEach((c) => {
+      if (entry_type_id === c.entry_type_id) result = c.hint;
+    });
 
     return result;
   }
 
-  updateEntry(id: number, params: EntryRequest) : Observable<StandardResponse<Entry>>{
+  updateEntry(
+    id: number,
+    params: EntryRequest
+  ): Observable<StandardResponse<Entry>> {
     const url = `${this.apiUrl}/${id}`;
-    
+
     return this.http.put<StandardResponse<Entry>>(url, params);
   }
 
-  deleteEntry(id: number) : Observable<StandardResponse<Entry>>{
+  deleteEntry(id: number): Observable<StandardResponse<Entry>> {
     const url = `${this.apiUrl}/${id}`;
-    
+
     return this.http.delete<StandardResponse<Entry>>(url);
   }
-
 }
