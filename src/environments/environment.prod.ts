ffa8977bd28192efe258c1a5ec4c8fc229723158
Mateusz Nowak <matteuszn65@gmail.com>
2023-06-28 12:30:15 +0200

    trying localhost ws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   tokenAllowedDomains: ['localhost:8080'],
   tokenDisallowedRoutes: ['http://localhost:8080/api/v1/auth/sign_in', 'http://localhost:8080/api/v1/auth/sign_up'],
   apiUrl: 'http://20.251.9.203/api/v1',
-  brokerURL: 'ws://20.251.9.203:8080/ws',
+  brokerURL: 'ws://localhost:8080/ws',
   defaultLanguage: 'pl',
   logLevel: NgxLoggerLevel.OFF,
   serverLogLevel: NgxLoggerLevel.OFF,
