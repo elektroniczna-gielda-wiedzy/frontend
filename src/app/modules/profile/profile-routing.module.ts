459f7117a9a56965dbe1883cd0564d5d50d22da5
Mateusz Nowak <matteuszn65@gmail.com>
2023-10-15 22:55:35 +0200

    Added profile details to navbar

--- src/app/modules/profile/profile-routing.module.ts
+++ src/app/modules/profile/profile-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileEntryListComponent } from './components/profile-entry-list/profile-entry-list.component';
+import { ProfileDetailsComponent } from './components/profile-details/profile-details.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: 'favorites',
     component: ProfileEntryListComponent,
     data: { page: 'favorites' },
+  },
+  {
+    path: 'details',
+    component: ProfileDetailsComponent
   }
 ];
 
