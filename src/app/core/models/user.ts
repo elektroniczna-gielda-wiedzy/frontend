ba296c22a897dcb98a7d4111df715cd7cdf50782
Mateusz Nowak <matteuszn65@gmail.com>
2023-10-15 21:20:30 +0200

    Added user info interface

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -1,18 +1,46 @@
 export interface UserSignInCredentials {
-    email: string;
-    password: string;
-    remember_me: boolean;
+  email: string;
+  password: string;
+  remember_me: boolean;
 }
 
 export interface UserSignUpCredentials {
-    email: string;
-    password: string;
-    first_name: string;
-    last_name: string;
+  email: string;
+  password: string;
+  first_name: string;
+  last_name: string;
 }
 
 export interface Author {
-    user_id: number;
-    first_name: string;
-    last_name: string;
-  }
+  user_id: number;
+  first_name: string;
+  last_name: string;
+}
+
+export interface UserInfo {
+  basic_info: Author;
+  email?: string;
+  created_at?: string;
+  last_login?: string;
+  activity?: {
+    no_entries: {
+      Announcement: number;
+      Note: number;
+      Post: number;
+    };
+    no_votes: {
+      Announcement: {
+        positive: number;
+        negative: number;
+      };
+      Note: {
+        positive: number;
+        negative: number;
+      };
+      Post: {
+        positive: number;
+        negative: number;
+      };
+    };
+  };
+}
