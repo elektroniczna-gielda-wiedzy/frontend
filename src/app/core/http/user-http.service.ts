8881b02253199110a18e6196b835181164b1c190
Mateusz Nowak <matteuszn65@gmail.com>
2023-10-15 22:51:51 +0200

    fixed again getUserInfo

--- src/app/core/http/user-http.service.ts
+++ src/app/core/http/user-http.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { UserInfo } from '../models/user';
+import { StandardResponse } from '../models/standard-response';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserHttpService {
   private readonly apiUrl = `${environment.apiUrl}/user`;
-  constructor(private http: HttpClient){ }
+  constructor(private http: HttpClient) {}
 
-  getUserInfo(userId: number) {
+  getUserInfo(userId: number): Observable<StandardResponse<UserInfo>> {
     const url = `${this.apiUrl}/${userId}`;
-    return this.http.get<UserInfo>(url);
+    const headers = { 'Content-Type': 'application/json' };
+    return this.http.get<StandardResponse<UserInfo>>(url, { headers });
   }
 }
