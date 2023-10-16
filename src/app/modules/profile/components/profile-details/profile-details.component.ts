22c465dc9c75a85631f6f989acbdb7ba95db90a3
Mateusz Nowak <matteuszn65@gmail.com>
2023-10-17 01:38:38 +0200

    Added loading other user for profile details

--- src/app/modules/profile/components/profile-details/profile-details.component.ts
+++ src/app/modules/profile/components/profile-details/profile-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TokenService, UserInfo } from 'src/app/core';
 import { UserHttpService } from 'src/app/core/http/user-http.service';
 
@@ -9,18 +10,35 @@ import { UserHttpService } from 'src/app/core/http/user-http.service';
 })
 export class ProfileDetailsComponent {
   userInfo?: UserInfo;
+  adminView = false; //TODO adjust admin view add ban & chat button, hide change password button
 
   constructor(
     private userHttpService: UserHttpService,
-    private tokenService: TokenService
+    private tokenService: TokenService,
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
-    const userId = this.tokenService.getUserId();
+    const currentUserId = this.tokenService.getUserId();
+    const url = this.router.url;
+    
+    if (url === '/profile/details') {
+      this.fetchUserInfo(currentUserId);
+    } else if (
+      url.startsWith('/admin-dashboard/users/') &&
+      this.tokenService.isAdmin()
+    ) {
+      this.adminView = true;
+      const userId = this.route.snapshot.paramMap.get('id');
+      this.fetchUserInfo(parseInt(userId || ''));
+    }
+  }
+
+  fetchUserInfo(userId: number | null) {
     if (!userId) {
       return;
     }
-
     this.userHttpService.getUserInfo(userId).subscribe((response) => {
       if (response.success && response.result?.length > 0) {
         this.userInfo = response.result[0];
