87fe34232d4d24c96e90bacf8957236671ba53de
Mateusz Nowak <matteuszn65@gmail.com>
2023-11-04 14:52:37 +0100

    Added result info to standard response

--- src/app/core/models/standard-response.ts
+++ src/app/core/models/standard-response.ts
@@ -1,5 +1,13 @@
 export interface StandardResponse<T> {
-    success: boolean;
-    messages: string[];
-    result: T[];
+  success: boolean;
+  messages: string[];
+  result: T[];
+  result_info?: ResultInfo;
+}
+
+export interface ResultInfo {
+  count: number;
+  page: number;
+  per_page: number;
+  total_count: number;
 }
